182a86911d48174a6ea4a84e056dda945e42c416
Rakesh Kumar <vrk4c@DESKTOP-JKEGSLC.fareast.corp.microsoft.com>
2020-11-01 17:41:35 +0000

    updated script for creating & managing sql server

--- Create SQL Server & DB.ps1
+++ Create SQL Server & DB.ps1
@@ -1,18 +1,3 @@
-#variable for keeping subscription count
-$subcount=1;
-
-#variable for having account details
-$AccDet= @();
-
-#variable which will have the subscription name
-$sub = "String";
-
-#variable for checking valid subscription provided
-$subcheck=1;
-
-#variable for Subscription details
-$subdetails=@();
-
 #variable for allocating random resource group name
 $resourceGroupName = "rg-$(Get-Random)";
 
@@ -21,13 +6,13 @@ $location = "westus2"
 
 #variable for setting an admin login and password for SQL server
 $adminSqlLogin = "SqlAdmin"
-$password = "ChangeYourAdminPassword1"
+$password = "Administrator1"
 
 #variable for server name - the logical server name has to be unique in the system
 $serverName = "server-$(Get-Random)"
 
 #variable for database name
-$databaseName = "SampleDatabase"
+$databaseName = "Sampledb001"
 
 #variable for The ip address range that you want to allow to access SQL server
 $startIp = "0.0.0.0"
@@ -45,80 +30,32 @@ $serverFirewallRule=@();
 #variable for getting info on created 
 $database=@();
 
-#connect Az account 
-Write-Host "******** Login to your Azure Account *********"
-
-#Taking account details
-$AccDet=Connect-AzAccount
-
-Get-AzSubscription | ForEach-Object {
-
-    Write-Host $subcount " -- " $_.Name;
-        
-    #variable which will have the subscription name
-    $sub=$_.Name;
-
-    $subcount++;
-
-}
-    
-if($subcount -eq 2) {
-    Write-Host "`nThere is only one subscription, which is `""$sub "`" and we are selecting it";
-} else {
-
-    DO {
-
-        $sub=Read-Host "Provide the Subscription Name "
-
-        Get-AzSubscription | ForEach-Object {
-
-            if($sub -contains $_.Name) {
-                $subcheck=2;
-            } 
-
-        }
-
-        if ($subcheck -eq 1) {
-            Write-Host "`nEnter valid subscription name" -ForegroundColor Red
-        }
-
-    } while($subcheck -eq 1)
-
-}
-
-Write-Host "`n"
-
-#variable for Subscription details
-$subdetails=Select-AzSubscription -SubscriptionName $sub
-$subdetails=Get-AzSubscription
-
-Write-Host "`nYou are in subscription :"$sub -ForegroundColor Green
-
-Write-Host "`nCreating resource group with name:"$resourceGroupName -ForegroundColor Yellow
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminSqlLogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force)
 
 # Create a resource group
 $resourceGroup = New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-Write-Host "`nCreated Resource group here is the info about it:`n"$resourceGroup -ForegroundColor Green
-
 Write-Host "`nCreating SQL server with name:"$serverName -ForegroundColor Yellow
 
 # Create a server with a system wide unique server name
-$server = New-AzSqlServer -ResourceGroupName $resourceGroupName -ServerName $serverName -Location $location -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminSqlLogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
+$server = New-AzSqlServer -ResourceGroupName $resourceGroupName -ServerName $serverName `
+            -Location $location -SqlAdministratorCredentials $cred
 
 Write-Host "`nCreated SQL Server here is the info about it:`n"$server -ForegroundColor Green
 
-Write-Host "`nUpdating Firewall rules for" $serverName "SQL server." -ForegroundColor Yellow
-
 # Create a server firewall rule that allows access from the specified IP range
-$serverFirewallRule = New-AzSqlServerFirewallRule -ResourceGroupName $resourceGroupName -ServerName $serverName -FirewallRuleName "AllowedIPs" -StartIpAddress $startIp -EndIpAddress $endIp
+$serverFirewallRule = New-AzSqlServerFirewallRule -ResourceGroupName $resourceGroupName `
+                                                  -ServerName $serverName -FirewallRuleName "AllowedIPs" `
+                                                  -StartIpAddress $startIp -EndIpAddress $endIp
 
-Write-Host "`nUpdated Firewall rules for" $serverName "SQL Server here is the info about it:`n"$serverFirewallRule -ForegroundColor Green
+Write-Host "`nHere are the details about Firewall rules"$serverFirewallRule -ForegroundColor Green
 
 Write-Host "`nCreating database with name" $databaseName "in" $serverName "SQL server" -ForegroundColor Yellow
 
 # Create a blank database with an S0 performance level
-$database = New-AzSqlDatabase  -ResourceGroupName $resourceGroupName -ServerName $serverName -DatabaseName $databaseName -RequestedServiceObjectiveName "S0" -SampleName "AdventureWorksLT"
+$database = New-AzSqlDatabase  -ResourceGroupName $resourceGroupName -ServerName $serverName `
+                                -DatabaseName $databaseName -RequestedServiceObjectiveName "S0" `
+                                -SampleName "AdventureWorksLT"
 
 Write-Host "`nCreated" $databaseName "Database" $serverName "in SQL Server here is the info about it:`n"$database -ForegroundColor Green
 
@@ -132,11 +69,18 @@ $MonitorParameters = @{
 }
 (Get-AzMetric @MonitorParameters -DetailedOutput).MetricValues
 
-# Scale the database performance to Standard S1
-$database = Set-AzSqlDatabase -ResourceGroupName $resourceGroupName -ServerName $servername -DatabaseName $databasename -Edition "Standard" -RequestedServiceObjectiveName "S1"
-
 # Set an alert rule to automatically monitor DTU in the future
-Add-AzMetricAlertRule -ResourceGroup $resourceGroupName -Name "MySampleAlertRule" -Location $location -TargetResourceId "/subscriptions/$($(Get-AzContext).Subscription.Id)/resourceGroups/$resourceGroupName/providers/Microsoft.Sql/servers/$serverName/databases/$databaseName" -MetricName "dtu_consumption_percent" -Operator "GreaterThan" -Threshold 90 -WindowSize $([TimeSpan]::Parse("00:05:00")) -TimeAggregationOperator "Average" -Action $(New-AzAlertRuleEmail -SendToServiceOwner)
+Add-AzMetricAlertRule -ResourceGroup $resourceGroupName -Name "MySampleAlertRule" -Location $location `
+                      -TargetResourceId "/subscriptions/$($(Get-AzContext).Subscription.Id)/resourceGroups/$resourceGroupName/providers/Microsoft.Sql/servers/$serverName/databases/$databaseName" `
+                      -MetricName "dtu_consumption_percent" -Operator "GreaterThan" -Threshold 90 `
+                      -WindowSize $([TimeSpan]::Parse("00:05:00")) -TimeAggregationOperator "Average" `
+                      -Action $(New-AzAlertRuleEmail -SendToServiceOwner)
+
+
+# Scale the database performance to Standard S1
+$database = Set-AzSqlDatabase -ResourceGroupName $resourceGroupName -ServerName $servername `
+                              -DatabaseName $databasename -Edition "Standard" `
+                              -RequestedServiceObjectiveName "S1"
 
 Write-Host "`nDeleting the resource group with name :" $resourceGroupName -ForegroundColor Yellow
 
