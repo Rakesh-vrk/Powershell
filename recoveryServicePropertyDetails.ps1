b97d2cb8640466b21bfc1df0d168da175189678f
Rakesh Kumar <vrk4c@DESKTOP-JKEGSLC.fareast.corp.microsoft.com>
2020-11-01 09:25:11 +0000

    script for getting soft delete details in recovery services

---
+++ recoveryServicePropertyDetails.ps1
@@ -0,0 +1,22 @@
+
+#If you wanted soft-delete option for a specific vault
+
+$vaultId = (Get-AzRecoveryServicesVault -VaultName "recovery-services" -ResourceGroupName 'frontend-servers-rg').id
+
+Get-AzRecoveryServicesVaultProperty -VaultId $vaultId
+
+
+#If you wanted soft-delete option for all vault 
+
+$vaults = Get-AzRecoveryServicesVault 
+
+foreach($vault in $vaults) { 
+    $properties = Get-AzRecoveryServicesVaultProperty -VaultId $vault.Id
+    if($properties.SoftDeleteFeatureState -eq 'Enabled') {
+        Write-Host "Soft delete feature is" $properties.SoftDeleteFeatureState "for" $vault.Name "`n" `
+                -ForeGroundColor Green
+    } else {
+        Write-Host "Soft delete feature is" $properties.SoftDeleteFeatureState "for" $vault.Name "`n" `
+                -ForeGroundColor Red
+    }
+ }
