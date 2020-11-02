d93833c07b7ce2a7bcbe27aff8ea010ec65ea995
Rakesh Kumar <vrk4c@DESKTOP-JKEGSLC.fareast.corp.microsoft.com>
2020-11-02 14:02:34 +0000

    Updated Script to check whether VM Agnet is installed

--- RunCommandsInVM/RunCommandsRemotely.ps1
+++ RunCommandsInVM/RunCommandsRemotely.ps1
@@ -1,21 +1,20 @@
-$vms =  ((Get-AzVM -ResourceGroupName "VRK-RG") | where-object { $_.OSProfile.WindowsConfiguration.ProvisionVMAgent -eq $True } ).Name
+$rg = "frontend-servers-rg"
+$vms =  (Get-AzVM -ResourceGroupName $rg).Name
 
 foreach($vm in $vms) {
-
-    $response = Invoke-AzVMRunCommand -ResourceGroupName 'VRK-RG' -VMName $vm -CommandId 'RunPowerShellScript' -ScriptPath 'C:\Users\v-ravent.FAREAST\Documents\VRK FILES DONT DELETE\Orcas Project\Powershell Scripts\git repo\RunCommandsInVM\WinAgent.ps1'
-
+    $count = 0;
+    Write-Host "`nChecking in $vm"
+    $response = Invoke-AzVMRunCommand -ResourceGroupName $rg -VMName $vm -CommandId 'RunPowerShellScript' -ScriptPath 'C:\Users\vrk4c\Documents\PowerShell\Scripts\RunCommandsInVM\WinAgent.ps1'
     $output = $response.value[0].message
-
     foreach ($string in $($output.Split("`n"))) {
         if($string -match '\w' -and !($string.startswith('Name'))) {
-        $file = $string.Split(' ').Trim() | Where-Object { $_ -match '\w'}
+            $file = $string.Split(' ').Trim() | Where-Object { $_ -match '\w'}
             if($file[0] -eq 'VMAgentPackage.zip') {
-                Write-Host "`nFound Vm Agent in $vm"
-                Write-Host $($file[0] -as [string]) 'is updated on' $($file[1].Split(" ")[0] -as [string]) -ForegroundColor Green
-            } else {
-                Write-Host "`n***VM agent doesnt exist in $vm" -ForegroundColor Red
-            }
+                $count++;
+                Write-Host $($file[0] -as [string]) 'is updated on' $($file[1].Split(" ")[0] -as [string])
+            } 
         } 
     }
-
+    if($count -eq 1) { Write-Host "Found Vm Agent in $vm" -ForegroundColor Green; } 
+    else { Write-Host "***VM agent doesnt exist in $vm" -ForegroundColor Red; }
 }
