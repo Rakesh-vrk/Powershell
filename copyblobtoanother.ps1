ef59d1180e421ad16e5a5b4a70438add2db7e4c0
Rakesh Kumar <vrk4c@DESKTOP-JKEGSLC.fareast.corp.microsoft.com>
2020-11-05 08:32:08 +0000

    script to copy blobs from one storage account to another

--- copyblobtoanother.ps1
+++ copyblobtoanother.ps1
@@ -1,23 +1,28 @@
+$rg = "rg-eastus"
 
-New-AzResourceGroup -Name rg-eastus -location eastus
+New-AzResourceGroup -Name $rg -location eastus
 
-New-AzStorageAccount -ResourceGroupName rg-eastus -AccountName stcacc008 -Location eastus -SkuName Standard_ZRS -Kind StorageV2
+New-AzStorageAccount -ResourceGroupName $rg -AccountName stcacc008 -Location eastus -SkuName Standard_ZRS -Kind StorageV2
 
-New-AzStorageAccount -ResourceGroupName rg-eastus -AccountName stcacc009 -Location eastus -SkuName Standard_ZRS -Kind StorageV2 -AsJob
+New-AzStorageAccount -ResourceGroupName $rg -AccountName stcacc009 -Location eastus -SkuName Standard_ZRS -Kind StorageV2 -AsJob
 
 $Acccontext = Get-AzContext
 
-azcopy.exe login --tenant-id $Acccontext.Tenant.Id
+.\azcopy.exe login --tenant-id $Acccontext.Tenant.Id
 
-$stacc1 = Get-AzStorageAccount -StorageAccountName stcacc008 -ResourceGroupName rg-eastus
-$stacc2 = Get-AzStorageAccount -StorageAccountName stcacc009 -ResourceGroupName rg-eastus
+$stacc1 = Get-AzStorageAccount -StorageAccountName stcacc008 -ResourceGroupName $rg
+$stacc2 = Get-AzStorageAccount -StorageAccountName stcacc009 -ResourceGroupName $rg
 
 $StaccSAS1 = New-AzStorageAccountSASToken -Service Blob,File,Table,Queue -ResourceType Service,Container,Object -Permission "racwdlup" -Context $stacc1.Context
 
 $StaccSAS2 = New-AzStorageAccountSASToken -Service Blob,File,Table,Queue -ResourceType Service,Container,Object -Permission "racwdlup" -Context $stacc2.Context
 
-New-AzStorageContainer -Name demo -Context $stacc1.Context -Permission Container
+$container= 'scripts'
 
-azcopy.exe copy "C:\Users\v-ravent.FAREAST\Documents\VRK FILES DONT DELETE\Orcas Project\Powershell Scripts\git repo\RunCommandsInVM\" "$($stacc1.Context.BlobEndPoint)demo/$staccSAS1" --recursive
+New-AzStorageContainer -Name $container -Context $stacc1.Context -Permission Container
 
-azcopy.exe copy "https://stcacc008.blob.core.windows.net/demo/$staccSAS1" "https://stcacc009.blob.core.windows.net/$StaccSAS2" --recursive
+.\azcopy.exe copy "C:\Users\vrk4c\Documents\PowerShell\Scripts\RunCommandsInVM\" "$($stacc1.Context.BlobEndPoint)$container/$staccSAS1" --recursive
+
+.\azcopy.exe copy "$($stacc1.Context.BlobEndPoint)$container/$staccSAS1" "https://stcacc009.blob.core.windows.net/$StaccSAS2" --recursive
+
+Get-AzResource -ResourceGroupName $rg | foreach { Remove-AzResource -ResourceGroupName $rg -ResourceName $_.Name -ResourceType $_.ResourceType -AsJob -Force }
